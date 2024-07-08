1455c4fd8121b5f306f498112dfd3088dada9d78
ahmdmnt <ahmd.mnt@gmail.com>
2024-07-09 01:16:26 +0300

    C++ Session01 Tasks

--- cppWorkspace/session01/task07.cpp
+++ cppWorkspace/session01/task07.cpp
@@ -2,21 +2,18 @@
  * Task 07:
  * Change from decimal to binary and vice versa
  */
-
 #include <iostream>
 #include <bitset>
 #include <ios>
 
 int main() {
-
     int inputNum;
     std::cout<< "Enter a Number: ";
     std::cin>> inputNum;
 
     std::cout<< std::showbase;
-    std::cout<< "Number in Bin format: " << std::bitset<16>{inputNum} << std::endl;
+    std::cout<< "Number in Bin format: " << std::bitset<16  >{inputNum} << std::endl;
     std::cout<< "Number in Oct format: " << std::oct << inputNum << std::dec << std::endl;
-    std::cout<< "Number in Hex format: " << std::hex << inputNum << std::dec << std::endl;   
-
+    std::cout<< "Number in Hex format: " << std::hex << inputNum << std::dec << std::endl;
     return 0;
 }
