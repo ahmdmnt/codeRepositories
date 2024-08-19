e4cf3ab9d04a463a65fdc30a5a4e2518c66cd4e2
ahmed-montasser1 <ahmed.montasser@seitech-solutions.com>
2024-08-20 00:40:56 +0300

    a

--- cppWorkspace/session03/myString.cpp
+++ cppWorkspace/session03/myString.cpp
@@ -3,23 +3,10 @@
 */
 
 #include <iostream>
-#include "myString.hpp"
+
+int main() {
 
 
-void myString::display () {
-   std::cout<< message << std::endl;
-}
 
-myString& myString::operator+=(const std::string& msg) {
-   this->message += msg;
-   return *this;   //myStringInstance
-}
-
-bool myString::operator<(const myString& dInstance) {
-   return this->message < dInstance.message;
-}
-
-
-std::string operator+(const myString& dInstance, const std::string& msg) {
-    return dInstance.message + msg;
+   return 0;
 }
--- cppWorkspace/session03/myString.hpp
+++ cppWorkspace/session03/myString.hpp
@@ -2,14 +2,4 @@
 * CLASS HEADER FILE: myString
 */
 
-class myString {
-public:
-    std::string message;
 
-    // Class Methods
-    void display ();
-    myString& operator+=(const std::string& msg);
-    bool operator<(const myString& dInstance);
-};
-
-std::string operator+(const myString& dInstance, const std::string& msg);
